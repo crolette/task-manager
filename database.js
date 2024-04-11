18729d27f7f1c39e10d0ea5ab32c49d82809dd23
crolette <crolweb@gmail.com>
2024-04-11 21:06:08 +0200

    Squashed commit of the following:
    commit 01a73e4c1a7b415dd59b06672135190e480f8614
    Author: crolette <crolweb@gmail.com>
    Date:   Sun Mar 24 21:23:19 2024 +0100
    
        update all db functions

---
+++ database.js
@@ -0,0 +1,114 @@
+// environnment variable
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+require('dotenv').config();
+DB_HOST = process.env.DB_HOST;
+DB_USERNAME = process.env.DB_USERNAME;
+DB_PASSWORD = process.env.DB_PASSWORD;
+DB_NAME = process.env.DB_NAME;
+
+// import mariadb from 'mariadb';
+const mariadb = require('mariadb');
+
+// create a connection to the database
+const connectToDatabase = async () => {
+	let conn;
+	try {
+		conn = await mariadb.createConnection({
+			host: DB_HOST,
+			user: DB_USERNAME,
+			password: DB_PASSWORD,
+			database: DB_NAME,
+			trace: true,
+		});
+	} catch (err) {
+		console.log(err);
+	}
+
+	return conn;
+};
+
+// create a connection to the database
+const connectToServer = async () => {
+	let conn;
+	try {
+		conn = await mariadb.createConnection({
+			host: DB_HOST,
+			user: DB_USERNAME,
+			password: DB_PASSWORD,
+			trace: true,
+		});
+	} catch (err) {
+		console.log(err);
+	}
+
+	return conn;
+};
+
+// disconnect from the database
+const endConnection = (conn) => {
+	if (conn) conn.end();
+};
+
+// check if database exists
+// CREATE DATABASE IF NOT EXISTS DBName;
+async function checkDatabase() {
+	let db;
+	try {
+		let conn = await connectToServer();
+		// db = await conn.query('CREATE DATABASE IF NOT EXISTS tasks_manager');
+		db = await conn.query(
+			`SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'tasks_manager'`
+		);
+		endConnection(conn);
+		return db;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function createDatabase() {
+	let db;
+	let conn;
+	try {
+		conn = await connectToServer();
+		db = await conn.query(`CREATE DATABASE IF NOT EXISTS tasks_manager`);
+		await createTables();
+		endConnection(conn);
+		return db;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function checkIfIdExists(id) {
+	try {
+		conn = await connectToDatabase();
+		db = await conn.query(`SELECT id FROM tasks WHERE id = ?`, [id]);
+		endConnection(conn);
+		return db;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function createTables() {
+	conn = await connectToDatabase();
+	await conn.query(
+		'CREATE TABLE IF NOT EXISTS tasks(id INT PRIMARY KEY AUTO_INCREMENT, description VARCHAR(30), done BOOLEAN NOT NULL DEFAULT false)'
+	);
+	// await conn.query(
+	// 	'CREATE TABLE IF NOT EXISTS tasks_done(id INT PRIMARY KEY AUTO_INCREMENT, description VARCHAR(30), done BOOLEAN NOT NULL DEFAULT false)'
+	// );
+	endConnection(conn);
+}
+
+module.exports = {
+	connectToDatabase,
+	endConnection,
+	createDatabase,
+	checkDatabase,
+	createDatabase,
+	checkIfIdExists,
+};
--- public/utils/setDatabase.js
+++ public/utils/setDatabase.js
@@ -3,13 +3,12 @@ const { connectToDatabase, endConnection } = require('./database');
 // import { connectToDatabase, endConnection } from './database.js';
 
 // function to set a tasks
-async function setTask(description, user_id) {
-	console.log('set Task');
+async function setTask(description) {
 	try {
 		const conn = await connectToDatabase();
 		const result = await conn.query(
-			`INSERT INTO tasks(description, user_id) VALUES(?, ?)`,
-			[description, user_id]
+			`INSERT INTO tasks(description) VALUES(?)`,
+			[description]
 		);
 		endConnection(conn);
 		return result;
@@ -20,7 +19,6 @@ async function setTask(description, user_id) {
 
 // function to delete a task based on the id
 async function deleteTask(id) {
-	console.log('deleteTask');
 	try {
 		const conn = await connectToDatabase();
 		const result = await conn.query('DELETE FROM tasks WHERE id = ?', [id]);
@@ -32,7 +30,6 @@ async function deleteTask(id) {
 }
 
 async function setDoneTask(id) {
-	console.log('done');
 	try {
 		const conn = await connectToDatabase();
 		const result = await conn.query(`UPDATE tasks SET done = ? WHERE id = ?`, [
